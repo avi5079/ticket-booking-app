9006387a47409620453671dd8e01682b31cb5f7f
avi5079 <aviral.srivastava2102@gmail.com>
2023-04-08 16:50:50 +0530

    home-screen-added

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_booking_app/screens/home_screen.dart';
 // import 'package:flutter/src/widgets/container.dart';
 // import 'package:flutter/src/widgets/framework.dart';
 
@@ -13,7 +14,7 @@ class BottomBar extends StatefulWidget {
 class _BottomBarState extends State<BottomBar> {
   int _selectedIndex = 0;
   static final List<Widget> _widgetOptions = <Widget>[
-    const Text("Home"),
+    const HomeScreen(),
     const Text("Search"),
     const Text("Tickets"),
     const Text("Profile"),
