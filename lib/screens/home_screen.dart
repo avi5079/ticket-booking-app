2bcbb5f3181cc38ba2f2e56e39336350c9f033da
avi5079 <aviral.srivastava2102@gmail.com>
2023-04-12 18:12:39 +0530

    home-screen-header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../utils/app_styles.dart';
 // import 'package:flutter/src/widgets/container.dart';
 // import 'package:flutter/src/widgets/framework.dart';
 
@@ -8,6 +10,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: Color(0xFFeeedf2));
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good Morning",
+                          style: Styles.headLineStyle3,
+                        ),
+                        const SizedBox(height: 5),
+                        Text("Book Tickets", style: Styles.headLineStyle),
+                      ],
+                    ),
+                    Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/logo.png"),
+                            )))
+                  ],
+                )
+              ]))
+        ],
+      ),
+    );
   }
 }
