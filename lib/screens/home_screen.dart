212ce3560ca097567b103fc9727028d878e51d95
avi5079 <aviral.srivastava2102@gmail.com>
2023-04-13 14:51:48 +0530

    search-box added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 // import 'package:flutter/cupertino.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 import '../utils/app_styles.dart';
 // import 'package:flutter/src/widgets/container.dart';
@@ -17,6 +19,7 @@ class HomeScreen extends StatelessWidget {
           Container(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(children: [
+                const Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -27,7 +30,7 @@ class HomeScreen extends StatelessWidget {
                           "Good Morning",
                           style: Styles.headLineStyle3,
                         ),
-                        const SizedBox(height: 5),
+                        const Gap(5),
                         Text("Book Tickets", style: Styles.headLineStyle),
                       ],
                     ),
@@ -41,6 +44,26 @@ class HomeScreen extends StatelessWidget {
                               image: AssetImage("assets/images/logo.png"),
                             )))
                   ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                          color: Color(0xFFBFC205)),
+                      const Gap(5),
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                      )
+                    ],
+                  ),
                 )
               ]))
         ],
